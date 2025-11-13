b3e8f2a97c8266b729d8e12a248137016e83c784
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-13 23:10:42 +0530

    fix: remove mediaCredits , add getMediaCredits media controllers

--- src/controllers/media.controllers.js
+++ src/controllers/media.controllers.js
@@ -29,7 +29,7 @@ export const getMediaList = async (req, res) => {
 
 export const getMediaDetails = async (req, res) => {
   const { media_type, id } = req.params;
-  // console.log(media_type, id);
+  // media_type : movie/tv/person
 
   const response = await fetch(
     `https://api.themoviedb.org/3/${media_type}/${id}?language=en-US`,
@@ -42,6 +42,25 @@ export const getMediaDetails = async (req, res) => {
 
   const data = await response.json();
   // console.log(data);
+
+  return res.status(200).json(new ApiResponse(200, "Success", data));
+};
+
+export const getMediaCredits = async (req, res) => {
+  const { media_type, id } = req.params;
+  // media cast
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${media_type}/${id}/credits?language=en-US`,
+    options
+  );
+
+  if (!response || response.success === false) {
+    console.log(response);
+    throw new ApiError(500, "Failed to fetch media-list from data provider");
+  }
+
+  const data = await response.json();
 
   return res.status(200).json(new ApiResponse(200, "Success", data));
 };
