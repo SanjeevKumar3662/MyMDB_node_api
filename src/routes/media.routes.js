b34cf0cfec4f44e133ceec75cbaebf34a8220929
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-13 23:16:18 +0530

    fix: refactored media_credits route to /media/credits/ route

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  getMediaCredits,
   getMediaDetails,
   getMediaList,
 } from "../controllers/media.controllers.js";
@@ -9,5 +10,6 @@ const router = Router();
 
 router.get("/list/:media_type/:list_type/:page", asyncHandler(getMediaList));
 router.get("/details/:media_type/:id", asyncHandler(getMediaDetails));
+router.get("/credits/:media_type/:id", asyncHandler(getMediaCredits));
 
 export default router;
