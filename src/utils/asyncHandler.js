cbe424544dc1d0d4da78b44f7be4fd4d474a843f
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-13 19:29:53 +0530

    feat: add asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+export const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    if (process.env.MODE === "DEV") {
+      console.error(error);
+    } else {
+      console.error(error.message);
+    }
+
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
