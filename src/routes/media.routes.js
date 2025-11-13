67b4c3de173aa18fca80237ca7a6fb3bcb70527d
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-13 22:43:07 +0530

    fix: refactored media_list route to /list route

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getMediaList } from "../controllers/media.controllers.js";
+import {
+  getMediaDetails,
+  getMediaList,
+} from "../controllers/media.controllers.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 
-router.get("/list/:media_type/:list_type/:page", getMediaList);
+router.get("/list/:media_type/:list_type/:page", asyncHandler(getMediaList));
+router.get("/details/:media_type/:id", asyncHandler(getMediaDetails));
 
 export default router;
