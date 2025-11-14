483a93d7abcac1bc4fd0271184a3962d33314718
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-14 12:24:29 +0530

    fix:(route) modified /search to /api/v1/media/search

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -3,6 +3,7 @@ import {
   getMediaCredits,
   getMediaDetails,
   getMediaList,
+  getSearchResults,
 } from "../controllers/media.controllers.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -11,5 +12,6 @@ const router = Router();
 router.get("/list/:media_type/:list_type/:page", asyncHandler(getMediaList));
 router.get("/details/:media_type/:id", asyncHandler(getMediaDetails));
 router.get("/credits/:media_type/:id", asyncHandler(getMediaCredits));
+router.get("/search/:query_type/:query/:page", asyncHandler(getSearchResults));
 
 export default router;
