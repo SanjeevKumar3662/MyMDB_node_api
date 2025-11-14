5cf3e04567174bd9d033bee186aff71a4e9469d3
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-14 13:40:02 +0530

    fix:(route) modified /media_content to /api/v1/media/content

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  getMediaContent,
   getMediaCredits,
   getMediaDetails,
   getMediaList,
@@ -13,5 +14,9 @@ router.get("/list/:media_type/:list_type/:page", asyncHandler(getMediaList));
 router.get("/details/:media_type/:id", asyncHandler(getMediaDetails));
 router.get("/credits/:media_type/:id", asyncHandler(getMediaCredits));
 router.get("/search/:query_type/:query/:page", asyncHandler(getSearchResults));
+router.get(
+  "/content/:media_type/:id/:content_type",
+  asyncHandler(getMediaContent)
+);
 
 export default router;
