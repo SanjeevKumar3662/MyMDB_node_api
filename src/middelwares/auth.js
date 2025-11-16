e90f1f43222edef2ca3c7111233d1975cd126885
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-16 21:42:18 +0530

    feat: add auth.js for accessToken authentication

---
+++ src/middelwares/auth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/apiError.js";
+
+export const authenticateAccessToken = (req, res, next) => {
+  try {
+    const { accessToken } = req.cookies;
+    // console.log(accessToken);
+
+    const user = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+    req.user = user;
+    next();
+    return;
+  } catch (error) {
+    throw new ApiError(401, "Unauthorised request");
+  }
+};
