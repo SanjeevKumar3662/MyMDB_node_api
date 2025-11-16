656d0bf8174f60c575878578302980c9c6efc52d
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-16 21:43:31 +0530

    feat(route): add /user/login route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+
+import { authenticateAccessToken } from "../middelwares/auth.js";
 
 const router = Router();
 
 router.post("/register", asyncHandler(registerUser));
+router.post("/login", asyncHandler(loginUser));
+
+router.get("/test", authenticateAccessToken, (req, res) => {
+  // console.log(req.user);
+  return res.status(200).json({ user: req.user });
+});
 
 export default router;
