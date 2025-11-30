b67e01d0cf3f32bc82866df09e1bc352565251c8
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-30 15:41:22 +0530

    feat(comment): add get comment route for fetching all comment for a tmdbId

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { authenticateAccessToken } from "../middelwares/auth.js";
-import { addCommentToMedia } from "../controllers/comment.controller.js";
+import {
+  addCommentToMedia,
+  getMediaComments,
+} from "../controllers/comment.controller.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 
 router.post("/", authenticateAccessToken, asyncHandler(addCommentToMedia));
+router.get("/", asyncHandler(getMediaComments));
 // router.patch("/", authenticateAccessToken, addCommentToMedia);
-// router.get("/", addCommentToMedia);
 // router.delete("/", authenticateAccessToken, addCommentToMedia);
 
 export default router;
